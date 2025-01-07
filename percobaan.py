5ac7653b48009b9e12559540f6a36ab4ef946f5a
Ariski <Ariski@ARISKI>
2025-01-07 09:08:29 +0700

    kedua

--- percobaan.py
+++ percobaan.py
@@ -1,2 +1,11 @@
-while True :
-    print("HAHAHAHA")
+modus = 0 
+jarak = 1
+angka = [2,3,4,3,4,4]
+for i in angka :
+    if angka[0] == i :
+        modus += 1
+        print(modus)
+
+print ("hello")
+
+    
